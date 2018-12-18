aa73918fe893c0f8f51bad8f9a6c63a4a7f05c8a
andzajan <andzajan@gmail.com>
2018-12-18 23:33:19 +0000

    Some small experiments

--- 02_activeBuzzer.py
+++ 02_activeBuzzer.py
@@ -3,17 +3,22 @@ import RPi.GPIO as GPIO
 import time
 
 BeepPin = 12    # pin12
+LedPin = 13
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)        # Numbers pins by physical location
 	GPIO.setup(BeepPin, GPIO.OUT)   # Set pin mode as output
 	GPIO.output(BeepPin, GPIO.HIGH) # Set pin to high(+3.3V) to off the beep
+	GPIO.setup(LedPin,  GPIO.OUT)
+	GPIO.output(LedPin, GPIO.LOW)
 
 def loop():
 	while True:
 		GPIO.output(BeepPin, GPIO.LOW)
-		time.sleep(0.1)
+		GPIO.output(LedPin, GPIO.HIGH)
+		time.sleep(0.5)
 		GPIO.output(BeepPin, GPIO.HIGH)
+		GPIO.output(LedPin, GPIO.LOW)
 		time.sleep(0.1)
 
 def destroy():
--- service.py
+++ service.py
@@ -12,29 +12,40 @@
 import RPi.GPIO as GPIO
 import time
 import random
+import os
 
-LedPin = [11, 13, 15]    # pin11
+LedPin = [11, 13, 15, 29, 31, 33, 35, 37]    # pin11
 count = 0
 length = [0.1, 0.2, 0.5, 1]
+lnum = [1,2,3,4]
 
 GPIO.setmode(GPIO.BOARD)       # Numbers pins by physical location
 
 GPIO.setup(LedPin, GPIO.OUT)   # Set pin mode as output
 GPIO.output(LedPin, GPIO.LOW) # Set pin to high(+3.3V) to off the led
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 try:
 	while True:
-		new_pin = random.sample (LedPin, 2)
+		new_lnum = random.sample (lnum,1)[0]
+		#print new_lnum
+		new_pin = random.sample (LedPin, new_lnum)
 		new_length = random.sample (length, 1)[0]
-		print new_length
-		print '...led on'
+		#print new_length
+		#print '...led on'
 		GPIO.output(new_pin, GPIO.HIGH)  # led on
 		time.sleep(new_length)
-		print 'led off...'
+		#print 'led off...'
 		GPIO.output(new_pin, GPIO.LOW) # led off
 		time.sleep(new_length)
 		count = count + 1
-except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the flowing code will be  executed.
-	#GPIO.output(LedPin, GPIO.HIGH)     # led off
-	GPIO.cleanup()                     # Release resource
+
+		if GPIO.input(10) == GPIO.HIGH:
+        		print("Button was pushed!")
+			GPIO.output (LedPin, GPIO.LOW)
+			GPIO.cleanup()
+			os.system ("shutdown -h now")
+except KeyboardInterrupt:
+#except if GPIO.input(10) == GPIO.HIGH:
+	GPIO.cleanup()
 	print "\n Blink cycles:", count
